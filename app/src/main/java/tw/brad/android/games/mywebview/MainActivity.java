7cb0ebdfdbb086c9e5f778540c563e2059130b16
Brad <brad@brad.tw>
2017-08-14 19:49:55 +0800

    v2

--- app/src/main/java/tw/brad/android/games/mywebview/MainActivity.java
+++ app/src/main/java/tw/brad/android/games/mywebview/MainActivity.java
@@ -1,13 +1,30 @@
 package tw.brad.android.games.mywebview;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webview);
+        initWebView();
+    }
+
+    private void initWebView(){
+        webView.loadUrl("http://www.tcca.org.tw");
+    }
+
+    public void test1(View view){
+        Uri uri = Uri.parse("http://www.tcca.org.tw");
+        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(it);
     }
 }
