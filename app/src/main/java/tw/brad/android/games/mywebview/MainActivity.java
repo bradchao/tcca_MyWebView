ab533fdd38815b7d06f006b233d416b9c161b6ec
Brad <brad@brad.tw>
2017-08-14 21:09:32 +0800

    v5

--- app/src/main/java/tw/brad/android/games/mywebview/MainActivity.java
+++ app/src/main/java/tw/brad/android/games/mywebview/MainActivity.java
@@ -1,7 +1,12 @@
 package tw.brad.android.games.mywebview;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,8 +21,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    123);
+        }else{
+            init();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        init();
+    }
+
+    private void init(){
         webView = (WebView)findViewById(R.id.webview);
         initWebView();
+
+        
+
     }
 
     private void initWebView(){
