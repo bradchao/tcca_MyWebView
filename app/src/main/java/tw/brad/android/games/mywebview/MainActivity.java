276a34e5a7ffa97c055e564dce67b4db58427232
Brad <brad@brad.tw>
2017-08-14 21:01:14 +0800

    v4

--- app/src/main/java/tw/brad/android/games/mywebview/MainActivity.java
+++ app/src/main/java/tw/brad/android/games/mywebview/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -22,16 +23,18 @@ public class MainActivity extends AppCompatActivity {
     private void initWebView(){
         WebViewClient client = new WebViewClient();
         webView.setWebViewClient(client);
+
+        WebSettings setting = webView.getSettings();
+        setting.setJavaScriptEnabled(true);
+
         // 1.
         //webView.loadUrl("http://www.tcca.org.tw");
         // 2.
-        webView.loadUrl("file:///android_asset/brad.html");
+        webView.loadUrl("file:///android_asset/mymap.html");
     }
 
     public void test1(View view){
-        Uri uri = Uri.parse("http://www.tcca.org.tw");
-        Intent it = new Intent(Intent.ACTION_VIEW, uri);
-        startActivity(it);
+        webView.loadUrl("javascript:test2('Brad')");
     }
     public void test2(View view){
         webView.goBack();
